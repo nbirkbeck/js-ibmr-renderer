4a492e04917834be1364d6a02210069bfe1b9fa2
Neil Birkbeck <birkbeck@belvedere.(none)>
2013-11-16 18:15:06 -0700

    About to move the render code into a separate function.

--- js/library/main.js
+++ js/library/main.js
@@ -16,14 +16,7 @@ Main = function() {
 	       goog.bind(function(data, textStatus) {
 		       this.setFragmentShader_(data);
 		   }, this));
-	    /*
-	    error: function(error) {
-		console.log('error');
-		console.log(error);
-	    }
-	    */
-	    //	    dataType: 'script'
-	       
+
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(75, 640/480, 0.1, 1000);
     this.camera.position.z = 1.5;
